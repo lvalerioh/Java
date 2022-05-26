8dc2e35c1cb247d12bbbd71f8cd88aa61f0736ff
valerio <lvaleriohuancah@gmail.com>
2022-05-26 16:33:24 -0500

    mi primer comit

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public  static void main(String args[])
     {
-        System.out.println("heloo world");
+        boolean b = true;
+        if (b == true)
+            System.out.println("Hi geek");
     }
 }
