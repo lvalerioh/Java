bdebae6c87695224537a7b47675e26a7ff78e135
valerio <lvaleriohuancah@gmail.com>
2022-05-26 16:37:01 -0500

    segundo commit

--- Main.java
+++ Main.java
@@ -4,5 +4,7 @@ public class Main {
         boolean b = true;
         if (b == true)
             System.out.println("Hi geek");
+
+        System.out.println("estado del segundo cambio");
     }
 }
